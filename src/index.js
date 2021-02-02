84b9da740a494500b3aea23fce45fa3fdd5eca35
popleteeva <m.popleteeva@gmail.com>
2021-02-02 10:41:32 +0300

    testing listeners

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './scss/index.scss'
 import {Excel} from './components/excel/Excel';
-import {Header} from './components/header/header';
-import {Toolbar} from './components/toolbar/toolbar';
-import {Table} from './components/table/table';
-import {Formula} from './components/formula/formula';
+import {Header} from './components/header/Header';
+import {Toolbar} from './components/toolbar/Toolbar';
+import {Table} from './components/table/Table';
+import {Formula} from './components/formula/Formula';
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
 })
 excel.render()
-excel.destroy()
+
